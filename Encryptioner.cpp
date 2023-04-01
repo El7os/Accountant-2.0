9e0c189e21105cfdbb147bb8b4b273e2291dbe63
Emin Yahya Arabacı <Pro_yahya_lol@hotmail.com>
2023-04-02 02:33:46 +0300

    Initial Commit

---
+++ Encryptioner.cpp
@@ -0,0 +1,14 @@
+#include "Encryptioner.h"
+#include "string"
+
+std::string Encryptioner::Encrypt(const std::string& Data)
+{
+	// To do þifrelenmiþ veriyi döndür.
+	return Data;
+}
+
+std::string Encryptioner::Decrypt(const std::string& Data)
+{
+	// To do þifrelenmiþ veriyi döndür.
+	return Data;
+}
---
+++ Encryptioner.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//class string;
+
+class Encryptioner
+{
+	static std::string Encrypt(const std::string& Data);
+
+	static std::string Decrypt(const std::string& Data);
+};
+
