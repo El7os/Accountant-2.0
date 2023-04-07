17c84e0384a35d53486c3effc0ce09b92b691816
Emin Yahya Arabacı <Pro_yahya_lol@hotmail.com>
2023-04-07 19:42:44 +0300

    New AES algorithm is implemented

--- EncryptionLayer/Source/Encryptioner.cpp
+++ EncryptionLayer/Source/Encryptioner.cpp
@@ -1,29 +1,60 @@
 #include "../Encryptioner.h"
+#include <iostream>
 #include <openssl/aes.h>
 
 
 std::string Encryptioner_AES::Encrypt(const std::string& Data, const std::string& Key)
 {
-    const unsigned char* RawData = reinterpret_cast<const unsigned char*>(Data.c_str());
-    const unsigned char* KeyPtr = reinterpret_cast<const unsigned char*>(Key.c_str());
-    unsigned char* CipherText = nullptr;
-    AESEncrypt(RawData, Data.length(), KeyPtr, nullptr, CipherText);
-    return std::string(reinterpret_cast<const char*>(CipherText), sizeof(CipherText));
+    /*std::string EncryptedData;
+    AES_KEY AesKey;
+    if (AES_set_encrypt_key((const unsigned char*)Key.c_str(), 128, &AesKey) < 0) {
+        throw std::runtime_error("Failed to set AES key");
+    }
+    EncryptedData.resize(Data.size() + AES_BLOCK_SIZE);
+    unsigned char* EncryptedDataPtr = (unsigned char*)EncryptedData.data();
+    const unsigned char* DataPtr = (const unsigned char*)Data.data();
+    int EncryptedDataLength = Data.length();
+    AES_cbc_encrypt(DataPtr, EncryptedDataPtr, Data.size(), &AesKey, EncryptedDataPtr, AES_ENCRYPT);
+    EncryptedData.resize(EncryptedDataLength);
+    return EncryptedData;*/
+    std::string EncryptedData;
+    AES_KEY AesKey;
+    if (AES_set_encrypt_key((const unsigned char*)Key.c_str(), 128, &AesKey) < 0) {
+        throw std::runtime_error("Failed to set AES key");
+    }
+    EncryptedData.resize(Data.size() + AES_BLOCK_SIZE);
+    unsigned char* EncryptedDataPtr = (unsigned char*)EncryptedData.data();
+    const unsigned char* DataPtr = (const unsigned char*)Data.data();
+    int EncryptedDataLength = Data.length();
+    AES_cbc_encrypt(DataPtr, EncryptedDataPtr, Data.size(), &AesKey, EncryptedDataPtr, AES_ENCRYPT);
+    EncryptedData.resize(EncryptedDataLength);
+    return EncryptedData;
 }
 
-std::string Encryptioner_AES::Decrypt(const std::string& Data, const std::string& Key)
+std::string Encryptioner_AES::Decrypt(const std::string& EncryptedData, const std::string& Key)
 {
-    return std::string();
-}
+    /*std::string DecryptedData;
+    AES_KEY AesKey;
+    if (AES_set_decrypt_key((const unsigned char*)Key.c_str(), 128, &AesKey) < 0) {
+        throw std::runtime_error("Failed to set AES key");
+    }
+    DecryptedData.resize(Data.size());
+    unsigned char* DecryptedDataPtr = (unsigned char*)DecryptedData.data();
+    const unsigned char* EncryptedDataPtr = (const unsigned char*)Data.data();
+    AES_cbc_encrypt(EncryptedDataPtr, DecryptedDataPtr, Data.size(), &AesKey, DecryptedDataPtr, AES_DECRYPT);
+    DecryptedData.resize(strlen(DecryptedData.c_str()));
+    return DecryptedData;*/
 
-void Encryptioner_AES::AESEncrypt(const unsigned char* plaintext, int plaintext_len, const unsigned char* key, unsigned char* iv, unsigned char* ciphertext) {
-    AES_KEY aes_key;
-    AES_set_encrypt_key(key, 128, &aes_key);
-    AES_cbc_encrypt(plaintext, ciphertext, plaintext_len, &aes_key, iv, AES_ENCRYPT);
-}
-
-void Encryptioner_AES::AESDecrypt(const unsigned char* ciphertext, int ciphertext_len, const unsigned char* key, unsigned char* iv, unsigned char* decryptedtext) {
-    AES_KEY aes_key;
-    AES_set_decrypt_key(key, 128, &aes_key);
-    AES_cbc_encrypt(ciphertext, decryptedtext, ciphertext_len, &aes_key, iv, AES_DECRYPT);
+    std::string DecryptedData;
+    AES_KEY AesKey;
+    if (AES_set_decrypt_key((const unsigned char*)Key.c_str(), 128, &AesKey) < 0) {
+        throw std::runtime_error("Failed to set AES key");
+    }
+    DecryptedData.resize(EncryptedData.size());
+    unsigned char* DecryptedDataPtr = (unsigned char*)DecryptedData.data();
+    const unsigned char* EncryptedDataPtr = (const unsigned char*)EncryptedData.data();
+    int DecryptedDataLength = EncryptedData.size();
+    AES_cbc_encrypt(EncryptedDataPtr, DecryptedDataPtr, EncryptedData.size(), &AesKey, DecryptedDataPtr, AES_DECRYPT);
+    DecryptedData.resize(DecryptedDataLength);
+    return DecryptedData;
 }
--- Launcher/Main.cpp
+++ Launcher/Main.cpp
@@ -1,8 +1,19 @@
-#pragma warning(disable : 4996)
+
+#include "..\EncryptionLayer\Encryptioner.h"
+#include <iostream>
 
 int main()
 {
+	Encryptioner_AES Encryptioner;
 
+	std::string Key = "Yahya";
+
+	std::string EncryptedData = Encryptioner.Encrypt("Benim Emin Yahya", Key);
+	std::string DecryptedData = Encryptioner.Decrypt(EncryptedData, Key);
+
+	std::cout << "Encryped Data : " << EncryptedData << std::endl;
+	std::cout << "Decryped Data : " << DecryptedData << std::endl;
+	
 	
 
 	return 0;
